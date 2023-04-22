7c79d4971070d56576e050af30ee26aad9921ca0
baburav-apte <130459390+baburav-apte@users.noreply.github.com>
2023-04-22 22:25:30 +0530

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -3,15 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABf1HAg98d85KL7rXEwzQcDnaDamyyfIU",
-  authDomain: "gitchat-731a8.firebaseapp.com",
-  projectId: "gitchat-731a8",
-  storageBucket: "gitchat-731a8.appspot.com",
-  messagingSenderId: "532790389021",
-  appId: "1:532790389021:web:f3fdc2555efc91267d99f4"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
